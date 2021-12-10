2f4551d85630611e52a20e5a5a19592ceeefdbbb
Arturo <sotorojasarturo@gmail.com>
2021-12-10 20:26:44 -0300

    mascota funciona perfecto

--- mascota.py
+++ mascota.py
@@ -61,8 +61,8 @@ class mascota:
             self.treeview.delete(*self.treeview.get_children())  # Elimina todos los rows del treeview
             for i in data:
                 self.treeview.insert("", "end", text=i[0],
-                                     values=(i[1] + " " + i[2] + " " + i[3].replace(" ", "\\ " )  + " " + i[4].replace(" ", "\\ " )
-                                             + " " + i[5]), iid=i[0], tags="rojo")
+                                     values=(i[1].replace(" ", "\\ ") + " " + i[2] + " " + i[3].replace(" ", "\\ ") +
+                                             " " + i[4].replace(" ", "\\ ") + " " + i[5]), iid=i[0], tags="rojo")
 
             self.data = data
 
@@ -74,7 +74,7 @@ class mascota:
             sql = """select mascota.id_mascota, mascota.nom_masc, especie.nom_esp, concat( dueno.nom_due,' ', 
             dueno.ape_due) as nombre , mascota.descrip_masc, date_format(mascota.fecha_nacimiento, "%Y-%m-%d")
             from mascota join dueno on mascota.id_dueno = dueno.id_dueno
-            join especie  on mascota.id_especie = especie.id_especie ;"""
+            join especie  on mascota.id_especie = especie.id_especie where mascota.id_mascota = %(id_mascota)s;"""
 
             row_data = self.db.run_select_filter(sql, {"id_mascota": self.treeview.focus()})[0]
             modificar(self.db, self, row_data)
@@ -104,7 +104,7 @@ class insertar:
         tk.Label(self.insert_datos, text="Nombre: ").place(x=10, y=10, width=150, height=20)
         tk.Label(self.insert_datos, text="Especie: ").place(x=10, y=40, width=150, height=20)
         tk.Label(self.insert_datos, text="Dueño: ").place(x=10, y=70, width=150, height=20)
-        tk.Label(self.insert_datos, text="Descripcion: ").place(x=10, y=100, width=150, height=20)
+        tk.Label(self.insert_datos, text="Descripción: ").place(x=10, y=100, width=150, height=20)
         tk.Label(self.insert_datos, text="Fecha Nacimiento: ").place(x=10, y=130, width=150, height=20)
 
     def __config_entry(self):
@@ -112,10 +112,10 @@ class insertar:
         self.entry_nombre.place(x=160, y=10, width=200, height=20)
         self.combo_especie = ttk.Combobox(self.insert_datos)
         self.combo_especie.place(x=160, y=40, width=200, height=20)
-        self.combo_especie["values"], self.ids = self.__fill_combo_especie()
+        self.combo_especie["values"], self.id_esp = self.__fill_combo_especie()
         self.combo_dueno = ttk.Combobox(self.insert_datos)
         self.combo_dueno.place(x=160, y=70, width=200, height=20)
-        self.combo_dueno["values"], self.ids = self.__fill_combo_dueno()
+        self.combo_dueno["values"], self.id_due = self.__fill_combo_dueno()
         self.entry_descripcion = tk.Entry(self.insert_datos)
         self.entry_descripcion.place(x=160, y=100, width=200, height=20)
         self.entry_fecha= tk.Entry(self.insert_datos)
@@ -128,8 +128,8 @@ class insertar:
     def __insertar(self):  # Insercion en la base de datos.
         sql = """insert into mascota (nom_masc, id_especie, id_dueno, descrip_masc, fecha_nacimiento ) 
             values (%(nom_mascota)s, %(id_especie)s, %(id_dueno)s, %(descrip_masc)s, %(fecha)s)"""
-        self.db.run_sql(sql, {"nom_mascota": self.entry_nombre.get(), "id_especie": self.combo_especie.current(),
-                              "id_dueno": self.combo_dueno.current(), "descrip_masc": self.entry_descripcion.get(),
+        self.db.run_sql(sql, {"nom_mascota": self.entry_nombre.get(), "id_especie": self.id_esp[self.combo_especie.current()],
+                              "id_dueno": self.id_due[self.combo_dueno.current()], "descrip_masc": self.entry_descripcion.get(),
                               "fecha": self.entry_fecha.get()})
         self.insert_datos.destroy()
         self.padre.llenar_treeview()
@@ -157,46 +157,59 @@ class modificar:
         self.config_button()
 
     def config_window(self):  # Settings
-        self.insert_datos.geometry('350x200')
-        self.insert_datos.title("Modificar Medico")
+        self.insert_datos.geometry('400x200')
+        self.insert_datos.title("Modificar Datos de Mascota")
         self.insert_datos.resizable(width=0, height=0)
 
-    def config_label(self):  # Labels
+    def config_label(self):  # Labels id, nombre, especie, dueño, descripcion, fecha
         tk.Label(self.insert_datos, text="Nombre: ").place(x=10, y=10, width=100, height=20)
-        tk.Label(self.insert_datos, text="Apellido: ").place(x=10, y=40, width=100, height=20)
-        tk.Label(self.insert_datos, text="Telefono: ").place(x=10, y=70, width=100, height=20)
-        tk.Label(self.insert_datos, text="Dirección: ").place(x=10, y=100, width=100, height=20)
-        tk.Label(self.insert_datos, text="Email: ").place(x=10, y=130, width=100, height=20)
+        tk.Label(self.insert_datos, text="Especie: ").place(x=10, y=40, width=100, height=20)
+        tk.Label(self.insert_datos, text="Dueño: ").place(x=10, y=70, width=100, height=20)
+        tk.Label(self.insert_datos, text="Descripción: ").place(x=10, y=100, width=100, height=20)
+        tk.Label(self.insert_datos, text="Fecha: ").place(x=10, y=130, width=100, height=20)
 
-    def config_entry(self):  # Se configuran los inputs
+    def config_entry(self):  # Se configuran los inputs   #id, nombre, especie, dueño, descripcion, fecha
         self.entry_nombre = tk.Entry(self.insert_datos)
-        self.entry_nombre.place(x=110, y=10, width=200, height=20)
+        self.entry_nombre.place(x=160, y=10, width=200, height=20)
         self.entry_nombre.insert(0, self.row_data[1])
-        self.entry_apellido = tk.Entry(self.insert_datos)
-        self.entry_apellido.place(x=110, y=40, width=200, height=20)
-        self.entry_apellido.insert(0, self.row_data[2])
-        self.entry_telefono = tk.Entry(self.insert_datos)
-        self.entry_telefono.place(x=110, y=70, width=200, height=20)
-        self.entry_telefono.insert(0, self.row_data[3])
-        self.entry_direccion = tk.Entry(self.insert_datos)
-        self.entry_direccion.place(x=110, y=100, width=200, height=20)
-        self.entry_direccion.insert(0, self.row_data[4])
-        self.entry_email = tk.Entry(self.insert_datos)
-        self.entry_email.place(x=110, y=130, width=200, height=20)
-        self.entry_email.insert(0, self.row_data[5])
+        self.combo_especie = ttk.Combobox(self.insert_datos)
+        self.combo_especie.place(x=160, y=40, width=200, height=20)
+        self.combo_especie["values"], self.id_esp = self.__fill_combo_especie()
+        self.combo_especie.insert(0, self.row_data[2])
+        self.combo_dueno = ttk.Combobox(self.insert_datos)
+        self.combo_dueno.place(x=160, y=70, width=200, height=20)
+        self.combo_dueno["values"], self.id_due = self.__fill_combo_dueno()
+        self.combo_dueno.insert(0, self.row_data[3])
+        self.entry_descripcion = tk.Entry(self.insert_datos)
+        self.entry_descripcion.place(x=160, y=100, width=200, height=20)
+        self.entry_descripcion.insert(0, self.row_data[4])
+        self.entry_fecha= tk.Entry(self.insert_datos)
+        self.entry_fecha.place(x=160, y=130, width=200, height=20)
+        self.entry_fecha.insert(0, self.row_data[5])
 
     def config_button(self):  # Botón aceptar, llama a la función modificar cuando es clickeado.
         tk.Button(self.insert_datos, text="Aceptar",
                   command=self.modificar).place(x=50, y=160, width=200, height=20)
 
     def modificar(self):  # Insercion en la base de datos.
-        sql = """update dueno set nom_due = %(nom_due)s, ape_due = %(ape_due)s,
-            telefono = %(telefono)s, direccion = %(direccion)s, email = %(email)s where id_dueno = %(id_dueno)s"""
-        self.db.run_sql(sql, {"nom_due": self.entry_nombre.get(), "direccion": self.entry_direccion.get(),
-                              "ape_due": self.entry_apellido.get(), "email": self.entry_email.get(),
-                              "telefono": int(self.entry_telefono.get()), "id_dueno": int(self.row_data[0])})
+
+        sql = """update mascota set nom_masc = %(nombre)s, id_especie = %(especie)s,
+                    id_dueno = %(dueno)s, descrip_masc = %(descripcion)s, fecha_nacimiento = %(fecha)s where id_mascota = %(id_mascota)s"""
+        self.db.run_sql(sql, {"nombre": self.entry_nombre.get(), "especie": int(self.id_esp[ self.combo_especie.current()]),
+                              "dueno": int(self.id_due[self.combo_dueno.current()]), "descripcion": self.entry_descripcion.get(),
+                              "fecha": self.entry_fecha.get(), "id_mascota": int(self.row_data[0])})
 
         self.insert_datos.destroy()
         self.padre.llenar_treeview()
+
+    def __fill_combo_especie(self):
+        sql = "select id_especie, nom_esp from especie"
+        self.data = self.db.run_select(sql)
+        return [i[1] for i in self.data], [i[0] for i in self.data]
+
+    def __fill_combo_dueno(self):
+        sql = "select id_dueno, concat(dueno.nom_due, ' ' , dueno.ape_due) as nombre from dueno;"
+        self.data = self.db.run_select(sql)
+        return [i[1] for i in self.data], [i[0] for i in self.data]
 
 
